4a319466a00fedbf7a57692454321086fcaf97a3
delson.modesto@unas.edu.pe <delson.modesto@unas.edu.pe>
2026-01-25 14:49:38 -0500

    ver precios de pasajes front

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { MainPage } from './layouts/pages/main.page/main.page';
+import { SelectRoutePage } from './features/seleccionderuta/pages/select-route.page/select-route.page';
 
 export const routes: Routes = [
     {
         path: '',
         component: MainPage
+    },
+    {
+        path: 'seleccionar-ruta',
+        component: SelectRoutePage
     }
     
 ];
