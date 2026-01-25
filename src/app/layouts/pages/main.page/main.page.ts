222df4c84c7f9543318948cbb63fab4340f1d743
EULERLOSVIN <eulerlosvinmartinezhurtado@gmail.com>
2026-01-25 14:43:40 -0500

    implement home

--- src/app/layouts/pages/main.page/main.page.ts
+++ src/app/layouts/pages/main.page/main.page.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from "@angular/router";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-main.page',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent],
   templateUrl: './main.page.html',
   styleUrl: './main.page.scss',
 })
