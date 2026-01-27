54b44073c268ea16244e48a972d034a03542d984
CharsDarwin <darwin.chamaya@unas.edu.pe>
2026-01-27 16:16:32 -0500

    validando pago

---
+++ src/app/features/FillData/components/validation-pay.component/validation-pay.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-validation-pay',
+  imports: [],
+  templateUrl: './validation-pay.component.html',
+  styleUrl: './validation-pay.component.scss',
+})
+export class ValidationPayComponent {
+
+}
--- src/app/features/FillData/pages/fill-data/fill-data.component.ts
+++ src/app/features/FillData/pages/fill-data/fill-data.component.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component, ViewChild } from '@angular/core';
 import { ModalPayComponent } from "../../components/modal-pay.component/modal-pay.component";
+import { ValidationPayComponent } from "../../components/validation-pay.component/validation-pay.component";
 
 @Component({
   selector: 'app-fill-data',
-  imports: [ModalPayComponent],
+  imports: [ModalPayComponent, ValidationPayComponent],
   templateUrl: './fill-data.component.html',
   styleUrl: './fill-data.component.scss',
   
